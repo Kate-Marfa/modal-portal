a4f6f3962a9f95def59a259601ba3ea505fdf46b
Kate-Marfa <k.marfichuk.gmail.com>
2025-07-25 16:47:01 +0300

    Added new style to textarea

--- src/ModalTextareaContent.js
+++ src/ModalTextareaContent.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 export function ModalTextareaContent({ text, setText }) {
-    return (
-        <textarea
-            rows={4}
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            style={{ width: '100%', padding: '8px' }}
-            placeholder="Type your text here..."
-        />
-    )
+  return (
+    <textarea
+      rows={4}
+      colums={5}
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      style={{ width: "100%", padding: "8px", width: "280px" }}
+      placeholder="Type your text here..."
+    />
+  );
 }
